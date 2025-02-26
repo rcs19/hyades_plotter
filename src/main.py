71a1c6e1c24d0a7e83d3efe91e65851eae4afe9c
rcs19 <ryan.saputil@gmail.com>
2025-02-26 22:39:25 +0000

    created jupyter workbook

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from scipy.io import netcdf_file
 from pathlib import Path
+import netCDF4
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,6 +7,5 @@ import matplotlib as mpl
 import os
 
 filepath = Path('hyades_output/SAI/hyImp35.cdf')
-data = netcdf_file(filepath) # Load in the output from the simulation
-for var in data.variables.keys():
-    print(var)
+data = netCDF4.Dataset(filepath)
+
